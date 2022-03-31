2171b43b71d9ae66e5999058d09955d714bd3204
nermine-elouni <73424389+nermine-elouni@users.noreply.github.com>
2022-03-31 14:18:35 +0200

    Revert "3d vis"
    This reverts commit eef4bbfa947aeb2f5d57bf452849b45eb4d355ba.

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const url= 'http://localhost:3000/posts';//returns the posts we have in DB
-export const fetchReviews = () => axios.get(url);
-export const createReview = (newReview) => axios.post(url, newReview);
-
