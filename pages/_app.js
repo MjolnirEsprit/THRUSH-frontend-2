5093cd62e24cb0d12aed730bdaf9fceec5b025c8
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2022-04-02 17:57:19 +0100

    organization commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,31 @@
 import "../public/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
-import {ThirdwebWeb3Provider} from '@3rdweb/hooks'
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 
-const supportedChainIds=[4] //Chain ID 4 represents Rinkeby network
-const connectors={
-    injected:{} //web3 connection method used by metamask here
-}
+const supportedChainIds = [4]; //Chain ID 4 represents Rinkeby network
+const connectors = {
+  injected: {}, //web3 connection method used by metamask here
+};
 const Noop = ({ children }) => <>{children}</>;
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const Layout = Component.Layout ?? Noop;
-  
+
   return (
-      <ThirdwebWeb3Provider
-          supportedChainIds={supportedChainIds}
-          connectors={connectors}
-      >
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
       <SessionProvider session={session}>
-          <RecoilRoot>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-          </RecoilRoot>
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
       </SessionProvider>
-      </ThirdwebWeb3Provider>);
+    </ThirdwebWeb3Provider>
+  );
 }
 
 export default MyApp;
