b6e93eb83c5dfd3013c850cfef5f25a1a799b507
firas <firas.benslama@esprit.tn>
2022-04-17 19:18:04 +0200

    cc

--- components/MusicStreaming/Sidebar.js
+++ components/MusicStreaming/Sidebar.js
@@ -10,7 +10,8 @@ import Image from "next/image";
 
 function Sidebar() {
   return (
-    <section className="fixed top-0 z-40 flex flex-col p-4 items-center bg-black w-[90px] h-screen space-y-8">
+    <section className="fixed top-0 z-40 sm:flex flex-col p-4 items-center w-[90px] h-screen space-y-8">
+                     
       <Image
           src="/public/vercel.svg"
           height={250}
@@ -20,11 +21,11 @@ function Sidebar() {
       />
       <div className="flex flex-col space-y-8">
         <HomeIcon className="sidebarIcon text-white opacity-[0.85]" />
-        <RiCompassFill className="sidebarIcon text-2xl" />
-        <FaMicrophoneAlt className="sidebarIcon ml-1" />
-        <ChartBarIcon className="sidebarIcon" />
-        <ClockIcon className="sidebarIcon" />
-        <DotsHorizontalIcon className="sidebarIcon" />
+        <RiCompassFill className="sidebarIcon text-white text-2xl" />
+        <FaMicrophoneAlt className="sidebarIcon text-white ml-1" />
+        <ChartBarIcon className="sidebarIcon text-white" />
+        <ClockIcon className="sidebarIcon text-white" />
+        <DotsHorizontalIcon className="sidebarIcon text-white" />
       </div>
     </section>
   );
