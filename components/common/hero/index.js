6892ad6f0957f6ca32266d3bb680fc1487f5e293
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2022-04-18 11:16:57 +0100

    Calzone 2

--- components/common/hero/index.js
+++ components/common/hero/index.js
@@ -4,11 +4,11 @@ export default function Hero() {
   return (
     <section className="lg:2/6 my-28 text-left">
       <div className="text-6xl font-semibold leading-none text-gray-900">
-        Grow your career as a developer
+        Grow your career as a musician
       </div>
       <div className="text-true-gray-500 mt-6 text-xl font-light antialiased">
-        Learn programming and web development the easy way! Get unlimited access
-        to all of our courses.
+        Learn music and instruments the easy way! Get exclusive access
+        to artists courses.
       </div>
       <div className="mt-5 flex sm:mt-8 lg:justify-start">
         <div className="rounded-md shadow">
