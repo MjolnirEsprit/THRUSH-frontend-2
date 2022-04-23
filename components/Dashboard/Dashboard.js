0d165e5ec9f1f9668fa9432e1954f0af0b8ff2d5
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2022-04-23 08:50:40 +0100

    chartjs

--- components/Dashboard/Dashboard.js
+++ components/Dashboard/Dashboard.js
@@ -23,19 +23,6 @@ import Deposits from './Deposits';
 import Orders from './Orders';
 import {userService} from "../../services";
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 const drawerWidth = 240;
 
 const AppBar = styled(MuiAppBar, {
