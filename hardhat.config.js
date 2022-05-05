b32b9cd2e94a7f0dd4f3825a3efeb53dcce5ec8f
nermine-elouni <73424389+nermine-elouni@users.noreply.github.com>
2022-05-05 19:18:26 +0100

    config of nft mp

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
     solidity: "0.8.4",
     paths: {
-        artifacts: "./NFTMarketplace/artifacts",
-        sources: "./NFTMarketplace/contracts",
-        cache: "./NFTMarketplace/cache",
-        tests: "./NFTMarketplace/test"
+        artifacts: "./components/NFTMarketplace/artifacts",
+        sources: "./components/NFTMarketplace/contracts",
+        cache: "./components/NFTMarketplace/cache",
+        tests: "./components/NFTMarketplace/test"
     }
 
 }
