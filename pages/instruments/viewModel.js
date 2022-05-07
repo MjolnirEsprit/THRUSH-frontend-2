13ea8edaa2b828c035be9e53eb38613ca6282bbc
nermine-elouni <73424389+nermine-elouni@users.noreply.github.com>
2022-05-07 15:35:14 +0100

    3d vis

--- pages/instruments/viewModel.js
+++ pages/instruments/viewModel.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BaseLayout } from "../../components/common/layout";
+<<<<<<< Updated upstream
 import {
     Button, Fab,
     Card, CardActionArea,
@@ -28,5 +29,60 @@ export default function ViewModel() {
 
 
 }
+=======
+import { Card } from '@material-ui/core';
+import "@google/model-viewer";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+const style = {
+    dispo: `flex w-full inline-block mt-5 justify-center items-center`,
+    itemm: `flex inline-block`
+}
+
+const itemData = [
+    {
+        mod: '/xiaotiqin.glb',
+        title: 'Camera',
+    },
+    {
+        mod: '/guitar.glb',
+        title: 'Breakfast',
+    },
+    {
+        mod: '/guitar2.glb',
+        title: 'Burger',
+    },
+    {
+        mod: '/gangqin.glb',
+        title: 'Burger',
+    },
+    {
+        mod: '/trumpet.gltf',
+        title: 'Camera',
+    }
+];
+
+export default function ViewModel() {
+
+    return (
+        <ImageList className={style.dispo} sx={{ height: 800 }} cols={3} rowHeight={164}>
+            {itemData.map((item) => {
+                return (
+                    <Card>
+                        
+                            <ImageListItem style={{ height: 320, width: 400 }} key={item.title}>
+                                <model-viewer className={style.itemm} style={{ height: 320, width: 400 }} camera-controls shadow-intensity="1" src={item.mod}></model-viewer>
+                            </ImageListItem>
+                        
+                    </Card>
+                )
+            })}
+        </ImageList>
+    );
+}
+
+
+>>>>>>> Stashed changes
 
 ViewModel.Layout = BaseLayout;
