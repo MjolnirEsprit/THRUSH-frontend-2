fea08d6d2d881199f2542d519a5b00f296eb4ea1
nermine-elouni <73424389+nermine-elouni@users.noreply.github.com>
2022-05-08 12:30:57 +0100

    commit

--- components/NFTMarketplace/scripts/deploy.js
+++ components/NFTMarketplace/scripts/deploy.js
@@ -1,14 +1,17 @@
--async function main() {
+async function main() {
     const [deployer] = await ethers.getSigners();
   
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
   
     
+    // Get the ContractFactories and Signers here.
     const NFT = await ethers.getContractFactory("NFT");
     const Marketplace = await ethers.getContractFactory("Marketplace");
+    // deploy contracts
     const marketplace = await Marketplace.deploy(1);
     const nft = await NFT.deploy();
+    // Save copies of each contracts abi and address to the frontend.
     saveFrontendFiles(marketplace , "Marketplace");
     saveFrontendFiles(nft , "NFT");
   }
